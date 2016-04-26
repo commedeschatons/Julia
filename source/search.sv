3dbbe74251e44bcd987bc9dc50ab6083f749487e
Mentor Graphics <mg148@ecegrid-thin4.ecn.purdue.edu>
2016-04-26 17:19:17 -0400

    Mux and search that soulja boy

--- source/search.sv
+++ source/search.sv
@@ -18,16 +18,18 @@ module search
 	input wire [NUM_JULIA*8 -1:0] catpixels,
 	input wire [NUM_JULIA -1:0] done,
 	output reg found,
-	output reg [7:0] sel_data,
-	output reg [31:0] sel_address
-		
+	output reg [7:0] sel_data_syn,
+	output reg [31:0] sel_address_syn,
+	output reg [NUM_JULIA-1:0] mask
 	);	
+	
+	reg [7:0] sel_data;
+	reg [31:0] sel_address;
 
 	reg shift_enable;
-	reg [NUM_JULIA -1:0] mask;
+	//reg [NUM_JULIA -1:0] mask;
 	
-	//search state logic block
-	assign found = ~shift_enable;
+	//search state logic bloc
 	
 	
 	mask_gen #(NUM_JULIA) mg (
@@ -37,35 +39,86 @@ module search
 		.mask(mask)
 	);
 	
+	
+	
+	always_ff @(posedge clk, negedge n_rst) begin
+		if (n_rst == 0) begin
+			sel_data_syn <= '0;
+			sel_address_syn <= '0;
+			found <= 0;
+		end
+		else begin
+			sel_data_syn <= sel_data;
+			sel_address_syn <= sel_address;
+			found <= ~shift_enable;
+		end
+	
+	end
 	//address and pixel muxing
+
+	
+	
+	
+	
+	//pretty sure this becomes a latch lololol.
+	
 	genvar k;
+	always_comb begin
+		sel_address = '1;
+		sel_data = '1;
+		shift_enable = 1;
+		for (int k=0; k<NUM_JULIA; k=k+1) begin
+			if ((mask & done) == 1<<k) begin
+			
+			sel_address[31:0] = cataddresses[k*32 +: 32];
+				sel_data[7:0] = catpixels[k*8 +: 8];
+				shift_enable = 0;
+			
+			end
+		
+		end
 	
+	end
+	
+			//genvar k;
+	/*
 	generate
-	
-	for (k = 0; k < NUM_JULIA; k=k+1) begin
+
+	for (genvar k = 0; k < NUM_JULIA; k=k+1) begin
 	always @(mask) begin
 	sel_data = '1;
 	sel_address = '1;
 	shift_enable = 1;
 	
-	
-		
-		
 			if ((mask & done) == 1<<k) begin
 				
-				sel_address[31:0] = cataddresses[32*(k+1) -1: k*32];
-    			sel_data[7:0] = catpixels[8*(k+1) -1: k*8];
+				sel_address[31:0] = cataddresses[k*32 +: 32];
+				sel_data[7:0] = catpixels[k*8 +: 8];
+    			//sel_data[7:0] = catpixels[8*(k+1) -1: k*8];
 				shift_enable = 0;
+			
+			
 			end
-		
-		
-	
 	end
 	
 	end
 	endgenerate
-
-
 	
+	parameter X = 32;  
+
+	reg [X-1:0] onehot;
+	reg i_data [X];
+	reg  o_data;
+
+	always_comb begin
+	o_data = 'z;
+	for(int i = 0; i < X; i++) begin
+  	if(onehot == (1 << i))
+    o_data = cataddresses[32*i +: 32];
+end
+end
+
+*/
+
 	
 endmodule
---
+++ source/tb_search.sv
@@ -0,0 +1,100 @@
+// $Id: $
+// File name:   tb_eop_detect.sv
+// Created:     2/25/2016
+// Author:      Dimitri James
+// Lab Section: 337-05
+// Version:     1.0  Initial Design Entry
+// Description: 
+
+`timescale 1ns/10ps
+
+module tb_search ();
+
+	
+	
+	localparam CLOCK_PER = 2.5;
+	localparam CHECK_DELAY = 1;
+	localparam NB = 4;
+
+	reg clk;
+	reg n_rst;
+	reg se;
+	reg [NB-1:0] mask;
+	reg [NB*32 -1:0] ca = 'hFCDA9879BAC10FFFFAABECD98;
+	reg [NB*8 -1:0] cp = $random();
+	reg [NB-1:0] d = 4'b1000;
+	reg found;
+	reg [31:0] sas;
+	reg [7:0] sds;
+	
+
+
+	
+	search #(NB) mg (
+		.clk(clk),
+		.n_rst(n_rst),
+		.cataddresses(ca),
+		.catpixels(cp),
+		.done(d),
+		.sel_data_syn(sds),
+		.sel_address_syn(sas),
+		.found(found),
+		.mask(mask)
+	);
+
+
+	// Generate clock.
+	always begin 
+		clk = 0;
+		#(CLOCK_PER / 2);
+		clk = 1;
+		#(CLOCK_PER / 2);
+	end
+
+	initial begin
+
+		n_rst=0;
+		@(negedge clk);
+		//lets try some more clk periods, see wrap around
+		n_rst = 1;
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		//12 clk periods
+		//lemme see that pause tho
+		@(negedge clk);
+		se = 0;
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		@(negedge clk);
+		se = 1;
+		@(posedge clk);
+		@(posedge clk);
+		
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+	
+	end
+	
+endmodule
